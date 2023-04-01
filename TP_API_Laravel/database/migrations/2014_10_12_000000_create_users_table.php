e0cb7e8fb45a170855749e2a3b4830fa4fb7677d
LoicDelas <loic.delas@gmail.com>
2023-04-01 14:49:44 -0400

    Modifications ordre des migrations

--- TP_API_Laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ TP_API_Laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('password');
-            $table->string('email')->unique();
+            $table->string('password', 255);
+            $table->string('email', 50)->unique();
             $table->string('last_name', 50);
             $table->string('first_name', 50);
-            $table->foreignId('role_id')->constrained();
             $table->rememberToken();
             $table->nullableTimestamps();
         });
