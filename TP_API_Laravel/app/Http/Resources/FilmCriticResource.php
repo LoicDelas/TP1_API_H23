c2cbc8dcfcadc7a933995e2e56675d63408a38b1
LoicDelas <loic.delas@gmail.com>
2023-04-04 18:00:28 -0400

    Ajout consultation film avec critique

---
+++ TP_API_Laravel/app/Http/Resources/FilmCriticResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FilmCriticResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'title' => $this->title,
+            'release_year' => $this->release_year,
+            'length' => $this->length,
+            'description' => $this->description,
+            'rating' => $this->rating,
+            'language_id' => $this->language_id,
+            'special_features' => $this->special_features,
+            'image' => $this->image,
+            'critics' => CriticResource::collection($this->critics)
+        ];
+    }
+}
